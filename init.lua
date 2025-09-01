e9e17c1e28b842370061c9b9fbffb301ce89602f
Scott Burns <scott@lentigo.net>
2025-09-01 10:52:19 -0400

    feat(options): set some more of my desired options

--- init.lua
+++ init.lua
@@ -1,12 +1,15 @@
+vim.o.autoindent = true
+vim.o.cursorline = true
 vim.o.expandtab = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.scrolloff = 10
 vim.o.shiftwidth = 4
 vim.o.signcolumn = "yes"
 vim.o.swapfile = false
 vim.o.tabstop = 4
-vim.o.wrap = false
 vim.o.winborder = "rounded"
+vim.o.wrap = false
 
 vim.g.mapleader = " "
 
