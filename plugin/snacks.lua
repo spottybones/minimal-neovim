3f8fbd6151ff6a215794a633efa58b146003dfeb
Scott Burns <scott@lentigo.net>
2025-09-04 11:09:34 -0400

    feat(keymaps): add descriptions to Snacks keymaps
    I also added a keymap for the keymaps picker. The definitions are
    displayed there. The keymap picker is helpful in quickly
    finding/learning keymaps.

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -8,8 +8,10 @@ require("snacks").setup({
 })
 
 -- keymaps for pickers
-vim.keymap.set("n", "<leader>f", Snacks.picker.smart)
-vim.keymap.set("n", "<leader>e", Snacks.picker.explorer)
-vim.keymap.set("n", "<leader>h", Snacks.picker.help)
-vim.keymap.set("n", "<leader>g", function() Snacks.lazygit() end)
-vim.keymap.set("n", "<leader>bd", function() Snacks.bufdelete() end)
+vim.keymap.set("n", "<leader>f", Snacks.picker.smart, { desc = "Pick Files" })
+vim.keymap.set("n", "<leader>e", Snacks.picker.explorer, { desc = "File Explorer" })
+vim.keymap.set("n", "<leader>h", Snacks.picker.help, { desc = "Pick Help" })
+vim.keymap.set("n", "<leader>u", Snacks.picker.buffers, { desc = "Pick Buffers" })
+vim.keymap.set("n", "<leader>k", Snacks.picker.keymaps, { desc = "Pick Keymaps" })
+vim.keymap.set("n", "<leader>g", function() Snacks.lazygit() end, { desc = "LazyGit" })
+vim.keymap.set("n", "<leader>bd", function() Snacks.bufdelete() end, { desc = "Delete Buffer" })
