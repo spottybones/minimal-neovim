3e1ca12e866570011ecabaefa2dd5e6b793e8c01
Scott Burns <scott@lentigo.net>
2025-10-20 19:22:19 -0400

    fix(lua): add vim and Mini globals for LuaLS

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -10,20 +10,31 @@
 --
 -- This config is designed for Lua's activity around Neovim. It provides only
 -- basic config and can be further improved.
+
+local globals = {
+  "vim",
+  "MiniDeps",
+  "MiniBasics",
+  "MiniCompletion",
+  "MiniExtra",
+  "MiniFiles",
+  "MiniIcons",
+  "MiniKeymap",
+  "MiniMax",
+  "MiniMisc",
+}
+
 return {
   on_attach = function(client, buf_id)
     -- Reduce very long list of triggers for better 'mini.completion' experience
-    client.server_capabilities.completionProvider.triggerCharacters =
-      { '.', ':', '#', '(' }
-
-    -- Use this function to define buffer-local mappings and behavior that depend
-    -- on attached client or only makes sense if there is language server attached.
+    client.server_capabilities.completionProvider.triggerCharacters = { ".", ":", "#", "(" }
   end,
   -- LuaLS Structure of these settings comes from LuaLS, not Neovim
   settings = {
     Lua = {
       -- Define runtime properties. Use 'LuaJIT', as it is built into Neovim.
-      runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
+      runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+      diagnostics = { globals = globals },
       workspace = {
         -- Don't analyze code from submodules
         ignoreSubmodules = true,
